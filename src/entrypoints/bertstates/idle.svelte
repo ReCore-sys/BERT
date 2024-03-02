cc3a4c9a0caa164ea59528168fede56b894886de
ReCore-sys <kjshore2004@gmail.com>
2024-03-02 17:15:14 +1030

    squooshed

--- src/entrypoints/bertstates/idle.svelte
+++ src/entrypoints/bertstates/idle.svelte
@@ -1,10 +1,29 @@
 <script lang="ts">
     // @ts-ignore
     import idle_bert from "../../assets/Bert_Idle.png";
+    import anime from "animejs";
+
+    function animatetheboy() {
+        anime({
+            targets: "#bert_idle_img",
+            scaleY: 0.8,
+            scaleX: 1.1,
+            duration: 2000,
+            loop: true,
+            direction: "alternate",
+            easing: "easeInOutSine",
+        });
+    }
 </script>
 
 <body>
-    <img src={idle_bert} alt="bert" class="logo" />
+    <img
+        src={idle_bert}
+        alt="bert"
+        class="logo"
+        id="bert_idle_img"
+        on:load={animatetheboy}
+    />
 </body>
 
 <style>
